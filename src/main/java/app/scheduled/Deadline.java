f04461b15f5b38fecc41735c651ea8b245803586
StanislavEleninWeb <stanislaveleninweb@gmail.com>
2019-12-19 10:55:52 +0200

    Add @component

--- src/main/java/app/scheduled/Deadline.java
+++ src/main/java/app/scheduled/Deadline.java
@@ -2,17 +2,23 @@ package app.scheduled;
 
 import java.nio.channels.InterruptedByTimeoutException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@Component
 @EnableAsync
 public class Deadline {
-	
+
+	Logger logger = LoggerFactory.getLogger(Deadline.class);
+
 	@Async
 	@Scheduled(fixedRate = 3600000)
-	public void scheduleDeadlineTaskCheck() throws InterruptedByTimeoutException {
-		
+	public void scheduledDeadlineTaskCheck() throws InterruptedByTimeoutException {
+
 	}
 
 }
