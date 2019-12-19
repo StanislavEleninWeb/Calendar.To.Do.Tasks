fbe56e1fc46103412c4edf0dd60025e837bf1b16
StanislavEleninWeb <stanislaveleninweb@gmail.com>
2019-12-19 10:18:10 +0200

    Replace DAO with Repository

--- src/main/java/app/dao/RoleDAO.java
+++
@@ -1,9 +0,0 @@
-package app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import app.entity.Role;
-
-public interface RoleDAO extends JpaRepository<Role, Long> {
-
-}
--- src/main/java/app/dao/TaskDAO.java
+++
@@ -1,9 +0,0 @@
-package app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import app.entity.Task;
-
-public interface TaskDAO extends JpaRepository<Task, Long>{
-
-}
