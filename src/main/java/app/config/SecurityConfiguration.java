117165d8478f9f9b58fed8eb43963a67db6a130b
StanislavEleninWeb <stanislaveleninweb@gmail.com>
2019-12-19 10:54:34 +0200

    Add permit all to register and forgotten password

--- src/main/java/app/config/SecurityConfiguration.java
+++ src/main/java/app/config/SecurityConfiguration.java
@@ -26,6 +26,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+		.antMatchers("/register", "/forgotten-password").permitAll()
 		.anyRequest().authenticated()
 		.and()
 		.formLogin()
